14dc56e80bd10d468a8099b8b70be06697ef7e95
Zach Williams <saumanahaii@gmail.com>
2017-05-15 14:56:24 -0400

    add missing data

--- src/hotColdContainer.js
+++ src/hotColdContainer.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 export default function hotCold(props){
   const guesses = props.guesses.map((guess,index)=>{
-    return (
-      <section>
+    return (<li key={index}>{guess}</li>);
+  });
 
+  return (
+
+    <main>
+      <section><h2>Message</h2></section>
+      <section>
+        <input type="text"></input>
+        <h3>Guess number {props.guesses[props.guesses.length-1]}</h3>
       </section>
-    );
-  })
+      <section><ul>{guesses}</ul></section>
+    </main>
+
+  )
 }
